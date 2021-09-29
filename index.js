29da800ab61e060f493f31e735be0aa6fddca683
Luke Bedrosian <bedrosian.luke05@gmail.com>
2021-09-29 19:51:39 -0400

    commit tree 40b7ceef02e86a6c7715e6a3eb760be2017f812e

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Add your code here
+function submitData(name, email){
+    const obj = {
+        "name" : name,
+        "email" : email,
+    };
+    return fetch ('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'Content-type' : 'application/json',
+            'Accept' : 'application/json',
+        },
+        body : JSON.stringify(obj)
+    })
+    .then(res => res.json())
+    .then(data => document.querySelector('body').append(data.id))
+    .catch(error => document.querySelector('body').append(error.message))
+    
+}
